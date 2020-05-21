d62a6da75e81128b6f99f09796ef74635062ba47
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-05-22 01:30:27 +0500

    Added CORS options

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // * Routes
-const user = require('./routes/user');
-const microApp1 = require('./routes/microApp1');
+const user = require('./routes/user')
+const microApp1 = require('./routes/microApp1')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
-  res.send('Server is running though');
-});
+  res.send('Server is running though')
+})
 
-app.use('/user', user);
-app.use('/data', microApp1);
+app.use('/user', user)
+app.use('/data', microApp1)
 
 app.listen(port, () => {
-  console.log(`We are live at 127.0.0.1:${port}`);
-});
+  console.log(`We are live at 127.0.0.1:${port}`)
+})
