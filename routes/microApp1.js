1e788d868f3024d34b235c623bea250f3932c2a0
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-05-22 14:56:26 +0500

    Add Api for fetching all logbook and reminder entries

--- routes/microApp1.js
+++ routes/microApp1.js
@@ -1,84 +1,143 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // * DB connection
-const { pool } = require('../services/db');
+const { pool } = require('../services/db')
 
 // * Auth Middleware
-const auth = require('../middleware/auth');
+const auth = require('../middleware/auth')
 
 // * Validations
 const {
   logbookFormValidation,
-  reminderFormValidation,
-} = require('../helpers/validations');
+  reminderFormValidation
+} = require('../helpers/validations')
 
+/* * *
+ * @method - GET
+ * @description - Get All Data Entries
+ * @param - /data/get-data-entries
+ */
+
+router.get('/get-data-entries', auth, async (req, res) => {
+  try {
+    // * request.user is getting fetched from Middleware after token authentication
+    pool.connect((err, client, done) => {
+      done()
+      const query = 'SELECT * FROM data order by id desc'
+      client.query(query, (error, result) => {
+        if (error) {
+          res.status(400).json({ error })
+        }
+        res.send({ entries: result.rows })
+      })
+    })
+  } catch (e) {
+    res.send({ message: 'Error in Fetching user' })
+  }
+})
+
+/* * *
+ * @method - POST
+ * @description - Add Data Entry
+ * @param - /data/add-data
+ */
 router.post('/add-data', auth, (req, res) => {
-  const { value, logbookEntry } = req.body;
-  let error = logbookFormValidation(req.body);
+  const { value, logbookEntry } = req.body
+  let error = logbookFormValidation(req.body)
   if (error) {
     return res.status(400).send({
-      error,
-    });
+      error
+    })
   } else {
     pool.connect((err, client, done) => {
-      done();
+      done()
       const query =
-        'INSERT INTO data(value, logbookEntry, createdAt, updatedAt) VALUES($1,$2,$3,$4) RETURNING *';
-      const values = [value, logbookEntry, Date.now(), Date.now()];
+        'INSERT INTO data(value, logbookEntry, createdAt, updatedAt) VALUES($1,$2,$3,$4) RETURNING *'
+      const values = [value, logbookEntry, Date.now(), Date.now()]
       client.query(query, values, (error, result) => {
         if (error) {
-          res.status(400).json({ error });
+          res.status(400).json({ error })
         }
-        const getAllValuesQuery = 'select * from data order by id desc';
+        const getAllValuesQuery = 'select * from data order by id desc'
         client.query(getAllValuesQuery, (error, selectQueryResult) => {
           if (error) {
-            res.status(400).json({ error });
+            res.status(400).json({ error })
           }
           res.status(200).send({
             status: 'Successfull',
             message: 'Entry Added Successfully',
-            entries: selectQueryResult.rows,
-          });
-        });
-      });
-    });
+            entries: selectQueryResult.rows
+          })
+        })
+      })
+    })
   }
-});
+})
+
+/* * *
+ * @method - GET
+ * @description - Get All Reminder Entries
+ * @param - /data/get-reminder-entries
+ */
+
+router.get('/get-reminder-entries', auth, async (req, res) => {
+  try {
+    // * request.user is getting fetched from Middleware after token authentication
+    pool.connect((err, client, done) => {
+      done()
+      const query = 'SELECT * FROM reminders order by id desc'
+      client.query(query, (error, result) => {
+        if (error) {
+          res.status(400).json({ error })
+        }
+        res.send({ entries: result.rows })
+      })
+    })
+  } catch (e) {
+    res.send({ message: 'Error in Fetching user' })
+  }
+})
+
+/* * *
+ * @method - POST
+ * @description - Add Reminder Entry
+ * @param - /data/add-reminder
+ */
 
 router.post('/add-reminder', auth, (req, res) => {
-  const { time, routine, description } = req.body;
-  let error = reminderFormValidation(req.body);
+  const { time, routine, description } = req.body
+  let error = reminderFormValidation(req.body)
   if (error) {
     return res.status(400).send({
-      error,
-    });
+      error
+    })
   } else {
     pool.connect((err, client, done) => {
-      done();
+      done()
       const query =
-        'INSERT INTO reminders(time, routine, description, createdAt, updatedAt) VALUES($1,$2,$3,$4,$5) RETURNING *';
-      const values = [time, routine, description, Date.now(), Date.now()];
+        'INSERT INTO reminders(time, routine, description, createdAt, updatedAt) VALUES($1,$2,$3,$4,$5) RETURNING *'
+      const values = [time, routine, description, Date.now(), Date.now()]
       client.query(query, values, (error, result) => {
         if (error) {
-          res.status(400).json({ error });
+          res.status(400).json({ error })
         }
-        const getAllValuesQuery = 'select * from reminders order by id desc';
+        const getAllValuesQuery = 'select * from reminders order by id desc'
         client.query(getAllValuesQuery, (error, selectQueryResult) => {
           if (error) {
             res.status(400).json({
-              error,
-            });
+              error
+            })
           }
           res.status(200).send({
             status: 'Successfull',
             message: 'Entry Added Successfully',
-            entries: selectQueryResult.rows,
-          });
-        });
-      });
-    });
+            entries: selectQueryResult.rows
+          })
+        })
+      })
+    })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
